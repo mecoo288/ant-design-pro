59d26c8d6210c9173750128349282c987ecb940a
PKAQ <pkaq@msn.com>
2019-09-09 17:43:39 +0800

    删除站内搜索

--- src/components/GlobalHeader/RightContent.tsx
+++ src/components/GlobalHeader/RightContent.tsx
@@ -25,29 +25,6 @@ const GlobalHeaderRight: React.SFC<GlobalHeaderRightProps> = props => {
 
   return (
     <div className={className}>
-      <HeaderSearch
-        className={`${styles.action} ${styles.search}`}
-        placeholder={formatMessage({
-          id: 'component.globalHeader.search',
-        })}
-        dataSource={[
-          formatMessage({
-            id: 'component.globalHeader.search.example1',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example2',
-          }),
-          formatMessage({
-            id: 'component.globalHeader.search.example3',
-          }),
-        ]}
-        onSearch={value => {
-          console.log('input', value);
-        }}
-        onPressEnter={value => {
-          console.log('enter', value);
-        }}
-      />
       <Tooltip
         title={formatMessage({
           id: 'component.globalHeader.help',
